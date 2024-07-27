85c78050e7e0ab3c474086e3a2d7f8ba045b7c24
piyeldev <84014136+piyeldev@users.noreply.github.com>
2024-07-27 15:08:35 +0800

    to publish in netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      edge: false,
+      split: false
+    })
   },
   preprocess: vitePreprocess()
 };
